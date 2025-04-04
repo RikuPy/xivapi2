581defc807f87acf2e3670ccaac70f546da6b34a
RikuPy <180808102+RikuPy@users.noreply.github.com>
2025-04-03 20:34:05 -0400

    Adds detailed docstrings for XivApiClient and related models

--- xivapi2/models.py
+++ xivapi2/models.py
@@ -1,8 +1,23 @@
 from dataclasses import dataclass
 
+__all__ = ["SearchResult", "SearchResponse", "RowResult", "SheetResponse", "Version"]
+
 
 @dataclass(kw_only=True, slots=True)
 class SearchResult:
+    """
+    Represents a single search result.
+
+    Attributes:
+        score (float): Relevance score for this entry. These values only loosely represent the relevance of an entry
+            to the search query. No guarantee is given that the discrete values, nor resulting sort order,
+            will remain stable
+        sheet (str): The name of the sheet this result was found in.
+        row_id (int): The ID of the row.
+        subrow_id (int | None): The subrow ID, when relevant.
+        fields (dict): The fields of the search result.
+        transient (dict | None): Transient data, when relevant.
+    """
     score: float
     sheet: str
     row_id: int
@@ -13,6 +28,13 @@ class SearchResult:
 
 @dataclass(kw_only=True)
 class SearchResponse:
+    """
+    Represents a search response. This can be iterated or indexed like a list.
+
+    Attributes:
+        schema (str): The schema of the search response.
+        results (list[SearchResult]): A list of search results.
+    """
     schema: str
     results: list[SearchResult]
 
@@ -31,6 +53,15 @@ class SearchResponse:
 
 @dataclass(kw_only=True, slots=True)
 class RowResult:
+    """
+    Represents a single row result.
+
+    Attributes:
+        row_id (int): The ID of the row.
+        subrow_id (int | None): The subrow ID, when relevant.
+        fields (dict): The fields of the row result.
+        transient (dict | None): Transient data, when relevant.
+    """
     row_id: int
     subrow_id: int | None = None
     fields: dict
@@ -39,6 +70,13 @@ class RowResult:
 
 @dataclass(kw_only=True)
 class SheetResponse:
+    """
+    Represents a sheet response. This can be iterated or indexed like a list.
+
+    Attributes:
+        schema (str): The schema of the sheet response.
+        rows (list[RowResult]): A list of row results.
+    """
     schema: str
     rows: list[RowResult]
 
@@ -58,14 +96,13 @@ class SheetResponse:
 @dataclass(slots=True)
 class Version:
     """
-    Represents a version of a sheet.
+    Represents a game version understood by the API.
     """
     names: list[str]
 
     def __str__(self):
         """
-        This is kind of a hacky simplification. Currently, the versions endpoint always returns lists of one
-        version element, but it's possible this may change in the future. So, I've created a dataclass to hold
-        the entire list of names in-case that does happen, but still allow str() casting for ease of reference.
+        Currently, the versions endpoint always returns lists of one version element, and casting this dataclass to a
+        string will return that first element by default.
         """
         return self.names[0]
